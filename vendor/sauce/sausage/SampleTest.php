db6acc57b49bb017478900553c2728ab67f7a8b0
Lakshmi Prasanna <lakshmi.prasanna@position2.com>
2014-07-30 12:07:20 +0530

    Create SampleTest.php

---
+++ vendor/sauce/sausage/SampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+class SampleTest extends Sauce\Sausage\WebDriverTestCase
+{
+
+    protected $start_url = 'http://position2.com';
+
+    public static $browsers = array(
+        // run FF15 on Windows 8 on Sauce
+        array(
+            'browserName' => 'firefox',
+            'desiredCapabilities' => array(
+                'version' => '15',
+                'platform' => 'Windows 2012',
+            )
+        ),
+ }
