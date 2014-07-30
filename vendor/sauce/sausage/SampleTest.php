3593607ffa3adb0a8db67101a6ebdfc56e85c30f
Lakshmi Prasanna <lakshmi.prasanna@position2.com>
2014-07-30 16:52:26 +0530

    Update SampleTest.php

--- vendor/sauce/sausage/SampleTest.php
+++ vendor/sauce/sausage/SampleTest.php
@@ -2,19 +2,49 @@
 
 require_once 'vendor/autoload.php';
 
-class SampleTest
-{
+require_once 'php-webdriver';
+$wd = new WebDriver();
+$session = $wd->session();
 
-    protected $start_url = 'http://position2.com';
+function cookies_contain($cookies, $name) {
+    foreach ($cookies as $arr) {
+        if ($arr['name'] == $name) {
+            return true;
+        }
+    }
+    return false;
+}
 
-    public static $browsers = array(
-        // run FF15 on Windows 8 on Sauce
-        array(
-            'browserName' => 'firefox',
-            'desiredCapabilities' => array(
-                'version' => '28',
-                'platform' => 'Windows 2012',
-            )
-        ),
-     );
+function get_cookie($cookies, $name) {
+    foreach ($cookies as $arr) {
+        if ($arr['name'] == $name) {
+            return $arr;
+        }
+    }
+    return false;
+}
+
+function alert_present($session) {
+    try {
+        $session->alert_text();
+        return true;
+    } catch (NoAlertOpenWebDriverError $e) {
+       return false;
+    }
+}
+
+function split_keys($toSend){
+    $payload = array("value" => preg_split("//u", $toSend, -1, PREG_SPLIT_NO_EMPTY));
+    return $payload;
+}
+
+$session->open("http://www.position2.com/");
+if (! (strpos($session->element("tag name", "html")->text(), "SOLUTIONS") !== false)) {
+    echo "verifyTextPresent failed";
+}
+$session->element("link text", "SOLUTIONS")->click();
+$session->element("xpath", "//header/div[1]/div/div[2]/div[2]/div/div/div/ul/li[2]/a")->click();
+$session->element("link text", "MARKETING AUTOMATION")->click();
+
+$session->close();
  }
