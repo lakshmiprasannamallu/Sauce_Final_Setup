8ce07298bd0fce93d955f97409efa75478d8fc9f
Lakshmi Prasanna <lakshmi.prasanna@position2.com>
2014-08-01 02:27:01 -0700

    Committed artima.php.

---
+++ vendor/sauce/sausage/artima.php
@@ -0,0 +1,107 @@
+{
+  "type": "script",
+  "seleniumVersion": "2",
+  "formatVersion": 2,
+  "steps": [
+    {
+      "type": "get",
+      "url": "http://www.artiman.com/"
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": ""
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": ""
+    },
+    {
+      "type": "goForward"
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": "We help build great companies, not just create a product or a quick flip. We take a long-term view o"
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": "Our entrepreneurs launch game-changing companies by pattern-matching experience, innovations, and best practices from one industry and applying them to another. Others have created disruptive combinations"
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": "In golf, the black tees are the ones furthest back and most challenging. We look for entrepreneurs that love the black tees -- who want to build great companies attacking markets of $10B or larger."
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": ""
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": ""
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": ""
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": ""
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": "Startups aren't easy - they're emotional roller coasters full of unpredictable twists and turns. Sometimes even \"good problems\" that come with rapid growth are challenging. We've all been there - Artiman's"
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": "Some of the most exciting disruptions come from technologies in the \"ampersand\" in R&D - proven research that hasn't yet been commercialized. All our partners have degrees in science and technology, and"
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": "Creating or disrupting huge markets also involves creating, coopting or disrupting existing value chains and ecosystems. We work proactively to build the targeted and eclectic network of partners that"
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": ""
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": ""
+    },
+    {
+      "type": "verifyTextPresent",
+      "text": ""
+    },
+    {
+      "type": "print",
+      "text": ""
+    },
+    {
+      "type": "clickElement",
+      "locator": {
+        "type": "css selector",
+        "value": "img[alt=\"White Space Investments\"]"
+      }
+    },
+    {
+      "type": "clickElement",
+      "locator": {
+        "type": "css selector",
+        "value": "img"
+      }
+    },
+    {
+      "type": "clickElement",
+      "locator": {
+        "type": "css selector",
+        "value": "img[alt=\"White Space Investments\"]"
+      }
+    }
+  ],
+  "data": {
+    "configs": {
+      "none": {}
+    },
+    "source": "none"
+  },
+  "inputs": [],
+  "timeoutSeconds": 60
+}
